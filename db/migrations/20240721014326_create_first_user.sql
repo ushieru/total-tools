34c9e24abe05a04028db1bcb1e42ec2ad56134c7
ushieru <uzielcocolan@gmail.com>
2024-08-26 23:49:50 -0600

    Fix(core): first user -> total tools

--- db/migrations/20240721014326_create_first_user.sql
+++ db/migrations/20240721014326_create_first_user.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-SELECT "total_tools_core"."create_user"('root','root', 'root', 'total_tools', 'root@total_tools.com', '0000000000', true);
+SELECT "total_tools_core"."create_user"('total','tools', 'Total', 'Tools', 'root@total_tools.com', '0000000000', true);
 
 -- migrate:down
 TRUNCATE TABLE "total_tools_core"."account" CASCADE;
